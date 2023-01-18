73609ec1cb088df383fb228da893c8159b1ed315
Zoltan Szabo <ZoltanZ.Szabo@endava.com>
2023-01-18 16:17:58 +0200

    compare result

--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+EXPECTED_RESULT=$1
+
+RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
@@ -33,7 +36,12 @@ for BRANCH in $(git for-each-ref --format='%(refname)' refs/heads/); do
     DIFF=$(($END - $START))
 
     TOTAL_TIME=$(($TOTAL_TIME + $DIFF))
-    echo "Run $i time: $DIFF"
+    echo -n "Run $i time: $DIFF"
+    if [ "$EXPECTED_RESULT" = "$CONTENT" ]; then
+      echo -e " - ${GREEN}OK${NC}"
+    else
+      echo -e " - ${RED}NOT OK${NC}"
+    fi
   done
 
   AVERAGE_TIME=$(($TOTAL_TIME/$TEST_RUNS))
