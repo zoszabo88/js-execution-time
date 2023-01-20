e090fdb877976c617b14aa6d302853b6d7333b86
Zoltan Szabo <ZoltanZ.Szabo@endava.com>
2023-01-20 12:29:31 +0200

    the big gun

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log(14);
+var fs = require('fs');
+var THE_RANDOM_STRING = fs.readFileSync('./input.txt', { encoding: 'utf-8', flag: 'r' }); // 100000014
+var RANGE = 14;
+
+/*
+Find the first range of 14 character that have no repeating characters and report the index after the range
+> index starting with 0
+
+Example:
+  - for a range of 5 instead of 14
+
+  The string: asasas112345Xasasasa
+
+  The unique string: 12345
+
+  Report the index of X: 12
+
+You can test by running: node main.js
+*/
+
+console.log('first');
