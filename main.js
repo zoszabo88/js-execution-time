919a708723ee5b94a013645ff36a1333577091da
bionut98 <borozan_ionut98@yahoo.com>
2023-04-25 17:09:04 +0300

    one iteration

--- main.js
+++ main.js
@@ -1,48 +1,27 @@
-function uniqueCharacters(str, evalA, len)
-    {
-        // Assuming string can have
-        // characters a-z this has
-        // 32 bits set to 0
-          
-        let checker = 0;
-        let i = 0;
-
-        while( i < str.length) {
-            let bitAtIndex = str[i].charCodeAt(0) - evalA;       
-            i++;  
-            // if that bit is already set
-            // in checker, return false
-            if ((checker & (1 << bitAtIndex)) > 0) {
-                return i;
-            }
-            // otherwise update and continue by
-            // setting that bit in the checker
-            checker = checker | (1 << bitAtIndex);
-        }
-  
-        // no duplicates encountered,
-        // return true
-        
-        return -1;
-    }
-
-
 var fs = require('fs');
 var THE_RANDOM_STRING = fs.readFileSync('./input.txt', { encoding: 'utf-8', flag: 'r' }); // 10000001va4
 // var THE_RANDOM_STRING = 'asasas112345Xasasasa';
 var RANGE = 14;
-const evalA = 'a'.charCodeAt(0);
+var evalA = 'a'.charCodeAt(0);
+var checker = 0;
+var counter = 0;
+var i = 0;
 
-var i = 0, len = THE_RANDOM_STRING.length;
-    while (i < len) {
-        var sub = THE_RANDOM_STRING.substring(i, i+RANGE);
-        var res = uniqueCharacters(sub, evalA)
-        if(res < 0) {
-          console.log(i+RANGE);
-          break;
-        }
-        i+=res-1;
-    }
+while(counter != RANGE) {
+  var bitAtIndex = THE_RANDOM_STRING[i++].charCodeAt(0) - evalA;   
+  
+  // if that bit is already set
+  // in checker, return false
+  
+  if ((checker & shiftRight(bitAtIndex)) > 0) {
+      counter = checker = 0;
+  }
+  counter++;
+  checker = checker | shiftRight(bitAtIndex);
+}
 
+function shiftRight(bitAtIndex) {
+  return 1 << bitAtIndex;
+}
 
-
+console.log(i);
