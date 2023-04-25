4ed8ec7fefce1da6192b2473be4a37a5741b76f0
bionut98 <borozan_ionut98@yahoo.com>
2023-04-25 14:45:15 +0300

    code cleanup

--- main.js
+++ main.js
@@ -33,23 +33,6 @@ var THE_RANDOM_STRING = fs.readFileSync('./input.txt', { encoding: 'utf-8', flag
 var RANGE = 14;
 const evalA = 'a'.charCodeAt(0);
 
-/*
-Find the first range of 14 character that have no repeating characters and report the index after the range
-> index starting with 0
-
-Example:
-  - for a range of 5 instead of 14
-
-  The string: asasas112345Xasasasa
-
-  The unique string: 12345
-
-  Report the index of X: 12
-
-You can test by running: node main.js
-*/
-
-
 var i = 0, len = THE_RANDOM_STRING.length;
     while (i < len) {
         var sub = THE_RANDOM_STRING.substring(i, i+RANGE);
@@ -61,8 +44,5 @@ var i = 0, len = THE_RANDOM_STRING.length;
         i+=res-1;
     }
 
-// var endTime = performance.now()
-
-// console.log(`Call to doSomething took ${endTime - startTime} milliseconds`)
 
 
