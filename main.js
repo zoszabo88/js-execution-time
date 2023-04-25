2b670038d4578085ab6c7eb3df0a472bba9266b1
Cristian baba <cristian.baba09@yahoo.com>
2023-04-25 13:49:55 +0300

    solution

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
-var fs = require('fs');
-var THE_RANDOM_STRING = fs.readFileSync('./input.txt', { encoding: 'utf-8', flag: 'r' }); // 100000014
-var RANGE = 14;
+var fs = require("fs");
+var THE_RANDOM_STRING = fs.readFileSync("./input.txt", {
+  encoding: "utf-8",
+  flag: "r",
+}); // 100000014
+var RANGE = 12;
+
+function findFirstNonRepeatingRange(randomString, range) {
+  for (let i = 0; i <= randomString.length - range; i++) {
+    if (new Set(randomString.slice(i, i + range)).size === range) {
+      return i + range;
+    }
+  }
+  return -1;
+}
+
+const indexAfterRange = findFirstNonRepeatingRange(THE_RANDOM_STRING, RANGE);
 
 /*
 Find the first range of 14 character that have no repeating characters and report the index after the range
 > index starting with 0
+
+
+
 
 Example:
   - for a range of 5 instead of 14
@@ -18,4 +35,4 @@ Example:
 You can test by running: node main.js
 */
 
-console.log('first');
+console.log(indexAfterRange);
