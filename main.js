7bbe05938de19f13b204d8c14af0063556090e19
Ganea Andreea Maria <ganea.andreea.maria@gmail.com>
2023-04-25 16:24:21 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 var fs = require("fs");
 var THE_RANDOM_STRING = fs.readFileSync("./input.txt", { encoding: "utf-8", flag: "r" }); // 100000014
 var RANGE = 14;
-let index = null;
-const maxRange = THE_RANDOM_STRING.length - RANGE - 1;
 
-let charSet = new Set();
+const maxRange = THE_RANDOM_STRING.length - RANGE - 1;
+let index = -1;
 let left = 0;
 let right = 0;
+let charSet = new Set();
 
 while (right < maxRange) {
   if (!charSet.has(THE_RANDOM_STRING[right]) && right - left + 1 <= RANGE) {
@@ -22,18 +22,5 @@ while (right < maxRange) {
     break;
   }
 }
-
-// for (let i = 0; i < maxRange; i++) {
-//   let charRange = [];
-//   for (let j = i; j < i + RANGE; j++) {
-//     charRange.push(THE_RANDOM_STRING[j]);
-//   }
-
-//   let newSet = new Set(charRange);
-//   if (charRange.length === newSet.size) {
-//     index = i + RANGE;
-//     break;
-//   }
-// }
 
 console.log(index);
