80fc38d4e30d3fb4c70fd08cbea03eedf203e766
bionut98 <borozan_ionut98@yahoo.com>
2023-04-25 14:31:54 +0300

    wrong index

--- main.js
+++ main.js
@@ -8,15 +8,13 @@ function uniqueCharacters(str, evalA, len)
         let i = 0;
 
         while( i < str.length) {
-            let bitAtIndex = str[i].charCodeAt(0) - evalA;
-            i++;
-  
+            let bitAtIndex = str[i].charCodeAt(0) - evalA;       
+            i++;  
             // if that bit is already set
             // in checker, return false
             if ((checker & (1 << bitAtIndex)) > 0) {
                 return i;
             }
-  
             // otherwise update and continue by
             // setting that bit in the checker
             checker = checker | (1 << bitAtIndex);
@@ -31,7 +29,7 @@ function uniqueCharacters(str, evalA, len)
 
 var fs = require('fs');
 var THE_RANDOM_STRING = fs.readFileSync('./input.txt', { encoding: 'utf-8', flag: 'r' }); // 10000001va4
-// var THE_RANDOM_STRING = 'asas112345Xasasasa';
+// var THE_RANDOM_STRING = 'asasas112345Xasasasa';
 var RANGE = 14;
 const evalA = 'a'.charCodeAt(0);
 
@@ -60,7 +58,7 @@ var i = 0, len = THE_RANDOM_STRING.length;
           console.log(i+RANGE);
           break;
         }
-        i+=res;
+        i+=res-1;
     }
 
 // var endTime = performance.now()
