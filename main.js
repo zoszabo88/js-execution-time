3d038a1739943d8bec6bde855b941c23f3731b5c
Iulian Toderesc <Iulian-George.Toderesc@endava.com>
2023-04-25 13:18:02 +0300

    solution for the frontend community challenge

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-var fs = require('fs');
-var THE_RANDOM_STRING = fs.readFileSync('./input.txt', { encoding: 'utf-8', flag: 'r' }); // 100000014
+const { log } = require("console");
+var fs = require("fs");
+var THE_RANDOM_STRING = fs.readFileSync("./input.txt", {
+	encoding: "utf-8",
+	flag: "r",
+}); // 100000014
 var RANGE = 14;
 
 /*
@@ -17,5 +21,19 @@ Example:
 
 You can test by running: node main.js
 */
+let result_index = 0;
+let subarray = THE_RANDOM_STRING[0];
+for (let i = 1; i < THE_RANDOM_STRING.length; i++) {
+	result_index = i;
 
-console.log('first');
+	if (subarray.indexOf(THE_RANDOM_STRING[i]) > -1) {
+		i = i - subarray.length;
+		subarray = "";
+	} else {
+		subarray += THE_RANDOM_STRING[i];
+		if (subarray.length == 14) {
+			console.log("INDEX resulted: " + (result_index + 1) + "\n");
+			break;
+		}
+	}
+}
